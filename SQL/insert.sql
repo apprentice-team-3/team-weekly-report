b39b98d2420a97f6b1df53566ada5dabb15c405a
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-21 14:03:45 +0900

    insert.sqlファイルのusresテーブル削除忘れ

--- SQL/insert.sql
+++ SQL/insert.sql
@@ -1,11 +1,3 @@
-CREATE TABLE users (
-    id INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    avatar_url VARCHAR(255),
-    created_at TIMESTAMP
-);
 INSERT INTO users (id, name, email, password, avatar_url) VALUES
 (1, 'YNSTakeru', 'ynstakeru@gmail.com', 'password', 'https://drive.google.com/file/d/1FXvhb0P416fJKwo-3_y8J3SmxDq65_kR/view?usp=sharing'),
 (2, 'gintoki', 'gintoki@gmail.com', 'password', ''),
