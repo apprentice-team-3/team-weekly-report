82fd79c67c8d7ed097999a964172cdfb5215ddb1
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-23 15:31:22 +0900

    parent_tasks, child_tasksのテーブルを踏まえてテーブルを設計

--- SQL/create.sql
+++ SQL/create.sql
@@ -6,7 +6,7 @@ use team_weakly_report;
 
 /*
 progressは0~100で、100になったら完了とする
-tasksテーブルのprogressから集計する
+parent_tasksテーブルのprogressから集計する
  */
 
 CREATE TABLE projects (
@@ -42,15 +42,14 @@ CREATE TABLE user_project (
 );
 
 
-
-CREATE TABLE tasks (
+/*
+progressはchild_tasksのprogressから集計する
+ */
+CREATE TABLE parent_tasks (
     project_id INT,
     user_id INT,
     id INT,
-    date DATE NOT NULL,
-    learning_time INT NOT NULL,
     title VARCHAR(128) NOT NULL,
-    content TEXT,
     progress INT DEFAULT 0 NOT NULL,
     created_at TIMESTAMP,
     PRIMARY KEY (project_id, user_id, id),
@@ -58,6 +57,20 @@ CREATE TABLE tasks (
     FOREIGN KEY (project_id) REFERENCES projects (id)
 );
 
+/*
+contentにtaskの詳細を記入
+ */
+CREATE TABLE child_tasks (
+    project_id INT,
+    user_id INT,
+    parent_task_id INT,
+    title VARCHAR(128) NOT NULL,
+    content TEXT,
+    progress INT DEFAULT 0 NOT NULL,
+    created_at TIMESTAMP,
+    PRIMARY KEY (project_id, user_id, parent_task_id, id),
+    FOREIGN KEY (project_id, user_id, parent_task_id) REFERENCES parent_tasks (project_id, user_id, id)
+);
 
 /*
 statusテーブル name候補
@@ -67,6 +80,7 @@ completed
 pending : 評価待ち
 evaluated
  */
+
 CREATE TABLE status(
     id INT PRIMARY KEY,
     name VARCHAR(128) NOT NULL,
@@ -80,15 +94,19 @@ CREATE TABLE task_status (
     status_id INT,
     isStatus BOOLEAN DEFAULT FALSE,
     primary key (project_id, user_id, task_id, status_id),
-    FOREIGN KEY (project_id, user_id, task_id) REFERENCES tasks (project_id, user_id, id),
+    FOREIGN KEY (project_id, user_id, task_id) REFERENCES child_tasks (project_id, user_id, parent_task_id, id),
     FOREIGN KEY (status_id) REFERENCES status (id)
 );
+
 
 /*
 メンバーのタスク評価項目を全て完了するとevaluationsテーブルに1行追加する
 ユーザーの総数を取得し
 evaluationsテーブルを使って、タスクの評価が完了したユーザーの総数を取得する
 evaluations評価が完了すると、isEvaluationをTRUEにする
+
+evaluationsテーブルのisEvaluationがTRUEにする条件
+メンバー全員が評価を完了した場合
  */
 
 CREATE TABLE evaluations(
@@ -97,9 +115,10 @@ CREATE TABLE evaluations(
     task_id INT,
     user_id INT,
     isEvaluated BOOLEAN DEFAULT FALSE,
+    comment VARCHAR(255),
     created_at TIMESTAMP,
     PRIMARY KEY (project_id, task_user_id, task_id, user_id),
-    FOREIGN KEY (project_id, task_user_id, task_id) REFERENCES tasks (project_id, user_id, id),
+    FOREIGN KEY (project_id, user_id, task_id) REFERENCES child_tasks (project_id, user_id, parent_task_id, id),
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
@@ -134,35 +153,12 @@ CREATE TABLE evaluation_tags(
     task_id INT,
     tag_name_id INT,
     PRIMARY KEY (project_id, task_user_id, task_id, tag_name_id),
-    FOREIGN KEY (project_id, task_user_id, task_id) REFERENCES tasks (project_id, user_id, id),
+    FOREIGN KEY (project_id, user_id, task_id) REFERENCES child_tasks (project_id, user_id, parent_task_id, id),
     FOREIGN KEY (tag_name_id) REFERENCES tags(id)
 );
 
--- scoreは各人で初期値に+1ポイント持っておくようにする
-
 /*
-evaluationsテーブルのisEvaluationがTRUEにする条件
-メンバー全員が評価を完了した場合
-レコード数がプロジェクトメンバーの数 * 3以上になっている
- */
-
-CREATE TABLE scores (
-    project_id INT,
-    task_id INT,
-    task_user_id INT,
-    user_id INT,
-    tag_name_id INT,
-    PRIMARY KEY (project_id, task_user_id, task_id, user_id, tag_name_id),
-    score INT DEFAULT 1 NOT NULL,
-    created_at TIMESTAMP,
-    Foreign Key (project_id, task_user_id, task_id) REFERENCES tasks (project_id, user_id, id),
-    Foreign Key (user_id) REFERENCES users (id),
-    Foreign Key (tag_name_id) REFERENCES tags(id)
-);
-
-
-/*
-以降はadvanced
+advanced 子タスクへのコメント
  */
 
 CREATE TABLE comments (
@@ -173,7 +169,7 @@ CREATE TABLE comments (
     comment TEXT NOT NULL,
     created_at TIMESTAMP,
     PRIMARY KEY (project_id, task_user_id, task_id, user_id),
-    FOREIGN KEY (project_id, task_user_id, task_id) REFERENCES tasks (project_id, user_id, id),
+    FOREIGN KEY (project_id, task_user_id, task_id) REFERENCES parent_tasks (project_id, user_id, id),
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
