3e59b3f78dba2492fb0bed07e859fe412fd9f810
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-26 00:38:28 +0900

    PHPからデータベースにアクセスする

---
+++ src/how-to-select-db.php
@@ -0,0 +1,23 @@
+<?php
+
+$user = "root";
+$pwd = "root";
+// ymlのサービス名を指定
+$host = "mysql";
+$dbName = "team_weekly_report";
+
+$dsn = "mysql:host={$host};port=3036;dbname={$dbName};port=3306;";
+
+
+$conn = new PDO("mysql:host={$host};dbname={$dbName};", $user, $pwd);
+
+// 連想配列で取得するように設定
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$pst = $conn->query('select * from users');
+$result = $pst->fetchAll();
+
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+
+$conn = null;
