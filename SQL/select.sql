4dd64f77bc780f017ce47eeda59a369f1974534a
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-25 18:49:51 +0900

    特定のプロジェクトに対して親タスクを集計してプロジェクト全体の進捗を取得し、集計した結果を特定のプロジェクトのprogressへ反映

--- SQL/select.sql
+++ SQL/select.sql
@@ -1,2 +1,10 @@
+-- 初期設定
+set @s_project_id = 1;
+
 -- デモで扱う分には固定のIDでもいいかも？
-SELECT title FROM projects p  where id = 1;
+SELECT title FROM projects p  where id = @s_project_id;
+
+-- 親タスクを集計してプロジェクト全体の進捗を出す
+SELECT  @s_project_progress := (sum(progress) / (count(progress) * 100)) FROM parent_tasks t where project_id = @s_project_id;
+
+UPDATE projects SET progress = @s_project_progress WHERE id = @s_project_id;
