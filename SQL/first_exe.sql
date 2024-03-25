72aed40a410f658f1e98adfd4739fbba9af04013
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-25 18:56:10 +0900

    exeファイルにプロジェクトの集計結果を反映させる

--- SQL/first_exe.sql
+++ SQL/first_exe.sql
@@ -273,3 +273,10 @@ INSERT INTO child_tasks (project_id, user_id, parent_task_id, id, title, content
 (1, 3, 7, 17, 'エラー時の処理', '', 100),
 (1, 4, 8, 18, '登録画面を作成', '', 100),
 (1, 4, 8, 19, '通信を暗号化する', '暗号化!!', 80);
+
+-- 初期設定
+set @s_project_id = 1;
+
+-- 親タスクを集計してプロジェクト全体の進捗を出す
+SELECT  @s_project_progress := (sum(progress) / (count(progress) * 100)) FROM parent_tasks t where project_id = @s_project_id;
+UPDATE projects SET progress = @s_project_progress WHERE id = @s_project_id;
--- SQL/select.sql
+++ SQL/select.sql
@@ -6,5 +6,4 @@ SELECT title FROM projects p  where id = @s_project_id;
 
 -- 親タスクを集計してプロジェクト全体の進捗を出す
 SELECT  @s_project_progress := (sum(progress) / (count(progress) * 100)) FROM parent_tasks t where project_id = @s_project_id;
-
 UPDATE projects SET progress = @s_project_progress WHERE id = @s_project_id;
