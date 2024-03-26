aeff8452901c00c8645f96b0931dea16520fef75
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-27 04:36:48 +0900

    ユーザーのデータを取得して表示する

--- src/member-task.php
+++ src/member-task.php
@@ -1,6 +1,7 @@
 <?php
 require_once "datasource.php";
 require_once "models/project.model.php";
+require_once "models/user.model.php";
 
 $title = 'メンバータスク一覧';
 $content = __DIR__ . '/views/member-task.php';
--- src/views/member-task.php
+++ src/views/member-task.php
@@ -2,6 +2,7 @@
 
 use db\DataSource;
 use model\Project;
+use model\User;
 
 try {
     $db = new DataSource;
@@ -10,13 +11,23 @@ try {
 
     $project = $db->selectOne($sql,[':id' => 1],DataSource::CLS,Project::class);
 
-
     $db->commit();
 } catch (PDOException $e){
-    echo '時間をおいて再度お試しください。<br>';
+    echo 'プロジェクトを取得できませんでした。<br>';
     $db->rollback();
 }
 
+try {
+    $db->begin();
+    $sql = 'SELECT * FROM users';
+
+    $users = $db->select($sql,[],DataSource::CLS,User::class);
+
+    $db->commit();
+} catch (PDOException $e) {
+    echo 'ユーザーを取得できませんでした。<br>';
+    $db->rollback();
+}
 ?>
 
 
@@ -32,4 +43,31 @@ try {
           完了
         </div>
     </h2>
+    <ul class="weekly__report__container">
+        <?php foreach ($users as $user) : ?>
+            <li class="weekly__report">
+               <div class="user__name">
+                   <?php echo $user->name ?>
+                </div>
+            </li>
+        <?php endforeach; ?>
+        <script>
+            const $users = document.querySelectorAll('.user__name')
+
+            // 文字の最大値を取得
+            const max = Math.max(...Array.from($users).map(user => user.textContent.length))
+
+            const maxValue = Array.from($users).find(user => user.textContent.length === max)
+            // 横幅のピクセルを取得
+            const width = maxValue.offsetWidth
+            const $weeklyReport = document.querySelector('.weekly__report')
+
+            const height = $weeklyReport.offsetHeight
+
+            Array.from($users).forEach(user => {
+                user.style.width = `${width}px`
+                user.style.height = `${height}px`
+            })
+        </script>
+    </ul>
 </div>
