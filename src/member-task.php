7b0e2b8ed5c867537634d5ce13dc45a55dec466f
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-27 03:08:38 +0900

    DBからprojectsのデータを取得してJSで整形後表示

--- src/member-task.php
+++ src/member-task.php
@@ -1,4 +1,7 @@
 <?php
+require_once "datasource.php";
+require_once "models/project.model.php";
+
 $title = 'メンバータスク一覧';
 $content = __DIR__ . '/views/member-task.php';
 include __DIR__ . '/views/layout.php';
--- src/views/member-task.php
+++ src/views/member-task.php
@@ -1,3 +1,35 @@
-<h2>
-    メンバータスク一覧画面
-</h2>
+<?php
+
+use db\DataSource;
+use model\Project;
+
+try {
+    $db = new DataSource;
+    $db->begin();
+    $sql = 'SELECT * FROM projects where id = :id;';
+
+    $project = $db->selectOne($sql,[':id' => 1],DataSource::CLS,Project::class);
+
+
+    $db->commit();
+} catch (PDOException $e){
+    echo '時間をおいて再度お試しください。<br>';
+    $db->rollback();
+}
+
+?>
+
+
+<div class="task__container">
+    <h2 class="project__container">
+        <div class="project__detail">
+          　<?php echo $project->title ?>
+          <script>
+            const progress = Number("<?php echo $project->progress ?>")
+            const progressPercent = progress.toFixed(4)
+            document.write(progressPercent + "%")
+          </script>
+          完了
+        </div>
+    </h2>
+</div>
