f6268ca24e0e6812986d0debaa450ef45e0d26bd
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-26 11:22:05 +0900

    projectモデルを作成

--- src/how-to-select-db.php
+++ src/how-to-select-db.php
@@ -1,14 +1,16 @@
 <?php
 require_once "datasource.php";
+require_once "models/project.model.php";
 
 use db\DataSource;
+use model\Project;
 
 try {
     $db = new DataSource;
     $db->begin();
     $sql = 'SELECT * FROM projects';
 
-    $result = $db->select($sql);
+    $result = $db->select($sql,[],DataSource::CLS,Project::class);
 
     echo "<pre>";
     print_r($result);
