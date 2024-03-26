1332f24f3707232565355b0bd133a8bc43b48808
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-26 11:22:30 +0900

    project.mode.phpファイルの追加

---
+++ src/models/project.model.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace model;
+
+class Project
+{
+    public int $id;
+    public string $title;
+    public string $content;
+    public float $progress;
+    public string $created_at;
+}
