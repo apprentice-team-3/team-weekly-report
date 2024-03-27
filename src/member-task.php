0845c1f00fdbf91247b680b6bffac9efc5704b76
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-27 15:00:09 +0900

    rebaseでマージ

--- src/member-task.php
+++ src/member-task.php
@@ -4,24 +4,7 @@ require_once "models/project.model.php";
 require_once "models/user.model.php";
 require_once "models/parent_task.model.php";
 require_once "models/weekly_tasks.model.php";
-
-use db\DataSource;
-use model\Project;
-
-try {
-    $db = new DataSource;
-    $db->begin();
-    $sql = 'SELECT * FROM projects where id = :id;';
-
-    $project = $db->selectOne($sql,[':id' => 1],DataSource::CLS,Project::class);
-
-
-    $db->commit();
-} catch (PDOException $e){
-    echo '時間をおいて再度お試しください。<br>';
-    $db->rollback();
-}
-
+require_once "services/common.php";
 
 $title = 'メンバータスク一覧';
 $content = __DIR__ . '/views/member-task.php';
---
+++ src/services/common.php
@@ -0,0 +1,64 @@
+<?php
+
+use db\DataSource;
+use model\Project;
+use model\User;
+use model\ParentTask;
+use model\WeeklyTask;
+
+try {
+    $db = new DataSource;
+    $db->begin();
+    $sql = 'SELECT * FROM projects where id = :id;';
+
+    $project = $db->selectOne($sql,[':id' => 1],DataSource::CLS,Project::class);
+
+
+    $db->commit();
+} catch (PDOException $e){
+    echo 'プロジェクトが取得できませんでした。再度時間をおいてお試しください。<br>';
+    $db->rollback();
+}
+
+
+try {
+    $db->begin();
+    $sql = 'SELECT * FROM users';
+
+    $users = $db->select($sql,[],DataSource::CLS,User::class);
+
+    $db->commit();
+} catch (PDOException $e) {
+    echo 'ユーザーを取得できませんでした。<br>';
+    $db->rollback();
+}
+
+try {
+    $db->begin();
+    // 全部とってくると重いので仮決めで200件にしている、本来はページネーションを使う
+    $sql = 'SELECT * FROM parent_tasks where project_id = :project_id ORDER BY created_at DESC LIMIT 200;';
+
+    $parent_tasks = $db->select($sql,[':project_id' => 1],DataSource::CLS,ParentTask::class);
+
+
+    $db->commit();
+    $weekly_tasks = [];
+
+    foreach($parent_tasks as $parent_task){
+        $date = $parent_task->getDate();
+        if(!isset($weekly_tasks[$date])){
+            $weekly_tasks[$date] = new WeeklyTask();
+        }
+        $weekly_tasks[$date]->addParentTask($parent_task);
+    }
+
+    // echo "<pre>";
+    // print_r($weekly_tasks);
+    // echo "</pre>";
+
+
+} catch (PDOException $e) {
+    echo '親タスクを取得できませんでした。<br>';
+    $db->rollback();
+
+}
