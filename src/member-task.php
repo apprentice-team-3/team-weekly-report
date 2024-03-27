30c278b949f76fd08609c84e73901843d6cfd295
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-27 12:14:46 +0900

    PHPの処理をviewsからお引越し

--- src/member-task.php
+++ src/member-task.php
@@ -2,6 +2,24 @@
 require_once "datasource.php";
 require_once "models/project.model.php";
 
+use db\DataSource;
+use model\Project;
+
+try {
+    $db = new DataSource;
+    $db->begin();
+    $sql = 'SELECT * FROM projects where id = :id;';
+
+    $project = $db->selectOne($sql,[':id' => 1],DataSource::CLS,Project::class);
+
+
+    $db->commit();
+} catch (PDOException $e){
+    echo '時間をおいて再度お試しください。<br>';
+    $db->rollback();
+}
+
+
 $title = 'メンバータスク一覧';
 $content = __DIR__ . '/views/member-task.php';
 include __DIR__ . '/views/layout.php';
--- src/views/member-task.php
+++ src/views/member-task.php
@@ -1,25 +1,3 @@
-<?php
-
-use db\DataSource;
-use model\Project;
-
-try {
-    $db = new DataSource;
-    $db->begin();
-    $sql = 'SELECT * FROM projects where id = :id;';
-
-    $project = $db->selectOne($sql,[':id' => 1],DataSource::CLS,Project::class);
-
-
-    $db->commit();
-} catch (PDOException $e){
-    echo '時間をおいて再度お試しください。<br>';
-    $db->rollback();
-}
-
-?>
-
-
 <div class="task__container">
     <h2 class="project__container">
         <div class="project__detail">
