56ad697688d413b565e1f70217ddf89d44a7f37e
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-29 03:13:46 +0900

    popup.phpを追加

---
+++ src/views/task-add-popup/task-add-popup-template.php
@@ -0,0 +1,18 @@
+<template id="task-add-template">
+    <div class="child__task__list__container">
+    <div class="popup__text child__task child__task__php">子タスク</div>
+    <div class="btn__container">
+        <button class="btn explanation">説明を表示</button>
+    </div>
+    <div class="comment__box hidden child__task__comment__php">子タスクについての説明</div>
+    <div class="progress__container">
+        <ul class="progress__character__container">
+        <li class="popup__text progress__character child__task__progress__0__php">0%</li>
+        <li class="popup__text progress__character red child__task__progress__30__php">30%</li>
+        <li class="popup__text progress__character yellow child__task__progress__60__php">60%</li>
+        <li class="popup__text progress__character blue child__task__progress__80__php">80%</li>
+        <li class="popup__text progress__character green child__task__progress__100__php">100%</li>
+        </ul>
+    </div>
+    </div>
+</template>
