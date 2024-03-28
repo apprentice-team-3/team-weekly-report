f803c66f122465da39c250f088a8d0620ba4dede
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-29 03:55:30 +0900

    ファイル追加忘れ？

---
+++ src/views/task-detail-popup/task-detail-popup-template.php
@@ -0,0 +1,48 @@
+<!-- views/popup.phpで呼ばれます -->
+<template id="task-edit-template">
+    <li class="child__task__list__container">
+    <div class="child__task__name__remove">
+        <div class="popup__text child__task child__task__php">子タスク</div>
+        <div class="icon icon__remove"></div>
+    </div>
+    <div class="comment__box child__task__comment__php">
+        <textarea
+        class="comment__textarea comment__php"
+        placeholder="サーバーからコメントを取得する"
+        ></textarea>
+    </div>
+        <div class="comment__box hidden child__task__comment__php">子タスクについての説明</div>
+    <div class="progress__container">
+        <div class="popup__text progress__explanation__character">
+        達成度を選択してください
+        </div>
+        <ul class="progress__character__container">
+        <li
+            class="popup__text progress__character child__task__progress__0__php selected"
+        >
+            <label for="">0%</label>
+        </li>
+        <li
+            class="popup__text progress__character red child__task__progress__30__php"
+        >
+            <label for="">30%</label>
+        </li>
+        <li
+            class="popup__text progress__character yellow child__task__progress__60__php"
+        >
+            <label class="" for="">60%</label>
+        </li>
+        <li
+            class="popup__text progress__character blue child__task__progress__80__php"
+        >
+            <label for="">80%</label>
+        </li>
+        <li
+            class="popup__text progress__character green child__task__progress__100__php"
+        >
+            <label for="">100%</label>
+        </li>
+        </ul>
+    </div>
+    </li>
+</template>
