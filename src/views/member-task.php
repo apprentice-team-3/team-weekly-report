1a9c55388c9db8163d549d4b1a722511851f68d1
Ryunosuke Matsuokaa <sirosakana014kenko@outlook.jp>
2024-03-28 23:50:41 +0900

    スタイルを整える

--- src/views/member-task.php
+++ src/views/member-task.php
@@ -33,19 +33,25 @@
                             ?>
                             <?php if ($weekly_task->date === $today) :?>
                                 <div class="date">
-                                    <?php echo $weekly_task->getDate() . ' 本日'; ?>
+                                    <div class="inner_date">
+                                        <?php echo $weekly_task->getDate() . ' 本日'; ?>
+                                    </div>
                                 </div>
                             <?php endif ; ?>
                             <?php if ($weekly_task->date !== $today) :?>
                                 <div class="date">
-                                    <?php echo $weekly_task->getDate(); ?>
+                                    <div class="inner_date">
+                                        <?php echo $weekly_task->getDate(); ?>
+                                    </div>
                                 </div>
                             <?php endif ; ?>
                             <div class="task">
                                 <?php for ($i = 0; $i < count($weekly_task->parent_tasks); $i++) :?>
-                                    <?php echo $weekly_task->parent_tasks[$i]->title; ?>
                                     <?php $progress = $weekly_task->parent_tasks[$i]->progress; ?>
-                                    <p><?php echo $progress; ?>%</p>
+                                    <div class="title_progress">
+                                        <div class="task_title"><?php echo $weekly_task->parent_tasks[$i]->title; ?></div>
+                                        <div class="task_progress"><?php echo $progress; ?>%</div>
+                                    </div>
                                     <div class="progress-bar">
                                         <div class="progress" style="width: <?php echo $progress; ?>%;"></div>
                                     </div>
